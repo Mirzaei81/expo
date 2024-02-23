72015c24a083822aff6fd52d70820aba0ae808bf
Kiarash <kiarasha@alum.mit.edu>
2024-02-23 10:03:01 -0600

    second

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,52 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { ScrollView, Text, View, Pressable} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import { Link } from 'expo-router';
+
+import { styled } from 'nativewind';
+import '../../assets/styles.css';
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 export default function TabOneScreen() {
+  const [selectedLanguage, setSelectedLanguage] = useState();
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <StyledView className="flex-2 items-center justify-center bg-white">
+      
+      <StyledView className="justify-center">
+      <StyledText className=" text-2xl m-12">Find what you’re looking for with just an image.</StyledText>
+      </StyledView>
+      <StyledText  className="text-l p-4">Select your language:</StyledText>
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="English" value="en" />
+        <Picker.Item label="Espanol" value="es" />
+      </Picker>
+
+
+
+      <button className="mt-20 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+      <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
+      <span>Start</span>
+      </button>
+      <StyledText className="text-l p-4">
+      Upload an image to begin your Search!
+      </StyledText>
+      <Link href="/modal" asChild>
+          <Pressable>
+              <StyledText className="text-xl font-bold text-blue-600 m-12">How It Works?</StyledText>
+         </Pressable>
+      </Link>
+      <Link href="/privacy policy" asChild>
+          <Pressable>
+              <StyledText className="text-small font-bold text-blue-600 mb-12">Privacy Policy</StyledText>
+         </Pressable>
+      </Link>
+    </StyledView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,282 @@
-import { StyleSheet } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image, ScrollView } from 'react-native';
+// import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
+import Spinner from 'react-native-loading-spinner-overlay';
+import { FlashList } from "@shopify/flash-list";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
+  // const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  // const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [imageWidth, setImageWidth] = useState(null);
+  const [imageHeight, setImageHeight] = useState(null);
+  const [resized, setResized] = useState(false);
+  const [statusMessage, setStatusMessage] = useState("Your picture is being uploaded now and then GPT will take a minute or two. Please wait!")
+  const [spinnerVisible, setSpinnerVisible] = useState(true);
+  // const [type, setType] = useState(Camera.Constants.Type.back);
+  const [queries, setQueries] = useState(null);
+  const [queriesTwo, setQueriesTwo] = useState(null);
+
+  // Image.prefetch("https://res.cloudinary.com/doic0dzx7/image/upload/w_500/r_100/v1708130148/bcubjmsofu123pvzdyjn.jpg");
+
+  function makeButton(data) {
+    const websearch = 'https://result.websearch-via-camera.com/en/'.concat(data);
+    return (
+      <TouchableOpacity style={{marginTop:20, marginBottom:20}} href={websearch}>
+        <Button  title={data}>
+        </Button>
+        </TouchableOpacity>
+    );
+}
+
+function _makeButton(item){
+  const websearch = 'https://result.websearch-via-camera.com/en/'.concat(item.item.value);
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <TouchableOpacity style={{marginTop:20, marginBottom:20}} href={websearch}>
+      <Button  title={item.item.value}>
+      </Button>
+      </TouchableOpacity>
   );
 }
 
+const PureCanvas = React.forwardRef((props, ref) => <canvas width={imageWidth} height={imageHeight} ref={ref} />);
+const canvasRef = useRef();
+
+useEffect(() => {
+  if (!image) return () => {};
+  const ctx = canvasRef.current.getContext("2d");
+
+  var img = new window.Image();
+  if (resized) {
+    img.onload = function() {
+      canvasRef.current.width = imageWidth;
+      canvasRef.current.height = imageHeight;
+      ctx.drawImage(this, 0, 0, imageWidth ,imageHeight);
+      upload();
+    }
+  } else {
+    img.onload = function() {
+      let newImageWidth = imageWidth;
+      let newImageHeight = imageHeight;
+      let ratio = 1;
+      if (imageWidth > imageHeight) ratio = 500/imageWidth;
+      if (imageWidth <= imageHeight) ratio = 500/imageHeight;
+
+      newImageHeight = imageHeight * ratio;
+      newImageWidth = imageWidth * ratio;
+
+      canvasRef.current.width = newImageWidth;
+      canvasRef.current.height = newImageHeight;
+      setImageWidth(newImageWidth);
+      setImageHeight(newImageHeight);
+      ctx.drawImage(this, 0, 0, newImageWidth ,newImageHeight);
+      setImage(canvasRef.current.toDataURL('image/png'))
+      setResized(true);
+    }
+  }
+  img.src = image;
+  return () => {};
+}, [image]);
+
+useEffect(() => {
+    (async () => {
+      // const  cameraStatus  = await Camera.requestCameraPermissionsAsync();
+      // setHasCameraPermission(cameraStatus.status === 'granted');
+      const  galleryStatus  = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === 'granted')
+      //setHasGalleryPermission(True);
+})();
+  }, []);
+// const takePicture = async () => {
+//     if(camera){
+//       const data = await camera.takePictureAsync(null);
+//       //console.log(data.uri)
+//       setImage(data.uri)
+      
+//     }
+//   }
+  
+ 
+  function upload() {
+    // let resized = resize();
+    const url = `https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-2b706faa-8009-4af8-9ba2-0d52f5a1bed1/default/doVision`;
+
+    fetch(url, {
+          method: "POST",
+          body: JSON.stringify({
+              base64: `${image}`
+            }),
+          headers: {
+              "Content-Type": "application/json"
+          }
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        // File uploaded successfully
+        // console.log(data);
+        setQueries(data);
+        setSpinnerVisible(false);
+      })
+      .catch((error) => {
+        setStatusMessage("Error uploading this file. Try with a different image!");
+        setSpinnerVisible(false);
+        console.error('Error uploading the file:', error);
+      });
+
+    const url2 = `https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-2b706faa-8009-4af8-9ba2-0d52f5a1bed1/default/doVision2`;
+
+    fetch(url2, {
+          method: "POST",
+          body: JSON.stringify({
+              base64: `${image}`
+            }),
+          headers: {
+              "Content-Type": "application/json"
+          }
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        // File uploaded successfully
+        // console.log(data);
+        setQueriesTwo(data);
+        setSpinnerVisible(false);
+      })
+      .catch((error) => {
+        setStatusMessage("Error uploading this file. Try with a different image!");
+        setSpinnerVisible(false);
+        console.error('Error uploading the file:', error);
+      });
+    
+  }
+
+const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      quality: 1,
+});
+
+setImage(result.uri);
+setImageWidth(result.width);
+setImageHeight(result.height);
+};
+
+if (hasGalleryPermission === false) {
+  return <View ><Text style={{padding:20}}>No access to photo gallery. </Text><TouchableOpacity href="https://websearch-via-camera.com"><Button title="Start over"></Button></TouchableOpacity></View>;
+}  
+
+// if (hasCameraPermission === false || hasGalleryPermission === false) {
+//     return <View ><Text style={{padding:20}}>No access to camera. </Text><TouchableOpacity href="https://websearch-via-camera.com"><Button title="Start over"></Button></TouchableOpacity></View>;
+// }
+
+const presetImage = (height) => {
+  if (height==750){
+    setQueries(['Avocado toast recipe', 'How to make avocado spread for bread', 'Healthy breakfast ideas', 'Egg and avocado toast', 'Avocado toast with boiled eggs', 'Simple avocado breakfast', 'Vegetarian breakfast recipes', 'Avocado and tomato on toast', 'Avocado toast variations', 'Nutritious breakfast with avocado and eggs']);
+    setQueriesTwo(['Slices of bread with avocado spread', 'A half of an avocado with the pit still inside', 'Two halves of a boiled egg with pepper sprinkled on top', 'Fresh cherry tomatoes', 'Wooden cutting board', 'Sprinkle of pepper and spices around the board', 'Sprigs of dill']);
+  }
+  if (height==281){
+    setQueries(['Sea otter with pup','Mother sea otter with baby', 'Sea otter habitat and lifestyle', 'Cute sea otter family', 'Sea otter conservation status']);
+    setQueriesTwo(['An adult sea otter', 'A baby sea otter (pup)', 'Water with ripples']);
+  }
+  if (height==306){
+    setQueries(['Ten Commandments in the Bible', 'Bible verses about not killing', 'Thou shalt not steal verse', 'Exodus 20 commandments text', 'Biblical text Thou shalt not bear false witness', 'Scripture on not committing adultery', 'Holy Bible commandments passage', 'Christian ethical teachings in Bible', 'Old Testament rules and commandments', 'Decalogue text in Christianity']);
+    setQueriesTwo(['A printed page from bible.', 
+    'Text, specifically what appears to be a numbered list of the Ten Commandments from the Bible.']);
+  }
+
+}
+
+
+return (
+  
+    <View style={styles.container}>
+      {!queries && !image && <Text style={{padding:20}}>GPT Vision can recognize general objects, scenes, colors, and shapes in the images. It can also answer questions about the content, context, and attributes of the images. For example, you can ask it what color a car is or what some ideas for dinner might be based on what is in your fridge.</Text>}
+      {/* {!queries && !image && 
+      <View style={{marginLeft:10, marginRight:10}}>
+        <Button
+        title="Flip Camera"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}>
+      </Button>
+      </View>
+      } */}
+      {/* {!queries && !image && <View style={{margin:10}}><Button title="Take Picture" onPress={() => takePicture()} /></View>} */}
+      {!queries && !image && <View style={{marginLeft:10, marginRight:10}}>
+          <Button title="Pick an Image From Gallery" onPress={() => pickImage()} />
+          <Text style={{margin: "auto",padding:20, fontSize:"2rem"}}>Pick an image from the gallery or try one of these:</Text>
+          <ScrollView >
+          
+          <TouchableOpacity style={{marginBottom:20}} onPress={() => presetImage(281)}>
+            <Image style={{margin: "auto", width:500,height:281,maxWidth:"100%", maxHeight:281}} source={{
+              uri: 'https://websearch-via-camera.com/camera/bcubjmsofu123pvzdyjn.jpeg',
+            }}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginBottom:20}} onPress={() =>presetImage(306)}>
+            <Image style={{margin: "auto", width:500,height:306,maxWidth:"100%", maxHeight:306}} source={{
+              uri: 'https://websearch-via-camera.com/camera/ebj8nlazyhgz3ucs6e6n.jpeg',
+            }}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginBottom:20}} onPress={() =>presetImage(750)}>
+            <Image style={{margin: "auto", width:500,height:750,maxWidth:"100%", maxHeight:750}} source={{
+              uri: 'https://websearch-via-camera.com/camera/mlkua19iskzugi1u27ch.jpeg',
+            }}/>
+          </TouchableOpacity>
+          
+          </ScrollView>
+        </View>}
+
+
+      {/* {!queries && !image && 
+      <View style={styles.cameraContainer}>
+        <Camera 
+          ref={ref => setCamera(ref)} 
+          style={styles.camera} 
+          type={type} 
+          ratio={'1:1'} 
+        />
+      </View>
+      } */}
+
+      {!queries && image && <View><Spinner visible={spinnerVisible}/>
+      <Text style={{padding:20}}>{statusMessage} </Text>
+      <TouchableOpacity href="https://websearch-via-camera.com/camera/"><Button title="Start over"></Button></TouchableOpacity>
+       <PureCanvas ref={canvasRef} /> 
+       </View>
+       }
+      {/* {!queries && image && <Button title="Reset" onPress={() => removeImage()} />} */}
+      {queries && queries.length > 0 && <Text style={{margin: "20px", fontSize: "large",fontWeight: "bold"}}>Search Intents:</Text>}
+      {queries && queries.length > 0 && <FlashList estimatedItemSize={100} data={[{value:'Title Text'}, {value:'Title Text2'}]} renderItem={_makeButton} />}
+
+      {queriesTwo && queriesTwo.length > 0 && <Text style={{margin: "20px", fontSize: "large",fontWeight: "bold"}}>Objects in the photo:</Text>}
+        {queriesTwo && queriesTwo.length > 0 && queriesTwo.map(makeButton, this)}
+      {(queriesTwo || queries) && <Text style={{margin: "20px", fontSize: "large",fontWeight: "bold"}}>Want to restart?</Text>}
+      {(queriesTwo || queries) && <TouchableOpacity href="https://websearch-via-camera.com/camera/"><Button title="Start over"></Button></TouchableOpacity>
+}
+      {queries && queries.length==0 && queriesTwo && queriesTwo.length==0 && <View ><Text style={{padding:20}}>No results for this image. </Text><TouchableOpacity href="https://websearch-via-camera.com/camera/"><Button title="Start over"></Button></TouchableOpacity></View>}
+
+    </View>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignContent: 'center'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  camera: {
+    flex: 1,
+    aspectRatio: 1,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  button: {
+    flex: 1
   },
 });
